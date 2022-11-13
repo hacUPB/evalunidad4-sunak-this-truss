08277eee33fc007b8b910e1277b2c51ddae4dc94
Juan <jdehoyos555@gmail.com>
2022-11-13 16:49:35 -0500

    creacion de cliente

---
+++ client.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#ifndef _CLIENT_H
+#define _CLIENT_H
+
+#define LENGTH 2048
+
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
+
+void str_overwrite_stdout() {
+
+}
+
+void str_trim_lf (char* arr, int length) {
+
+}
+
+void catch_ctrl_c_and_exit(int sig) {
+
+}
+
+void send_msg_handler() {
+
+}
+
+void recv_msg_handler() {
+
+}
+
+#endif
